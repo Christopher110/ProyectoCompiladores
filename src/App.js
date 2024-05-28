c4c243146a16acfb464c6ec18d1621e3d668e89d
CHRISTOPHER\chris <cramos@miumg.edu.gt>
2024-05-27 22:46:42 -0600

    Ajustes para las tablas de tokes y errores

--- src/App.js
+++ src/App.js
@@ -14,56 +14,93 @@ import { Container } from 'react-bootstrap';
 function App() {
   const [fileContent, setFileContent] = useState('');
   const [tokens, setTokens] = useState([]);
+  const [erroresLexicos, setErroresLexicos] = useState([]); // Agregando estado para errores léxicos
   const editorRef = useRef(null);
+
 
   const onFileUpload = (fileContent) => {
     setFileContent(fileContent);
   };
 
   function validarTexto() {
-    // Obtenemos el texto ingresado en el textbox
+    // Obtenemos el texto ingresado en el editor
     var texto = editorRef.current.editor.getValue();
-
-    console.log(texto)
-
-    // Expresión regular para validar el texto
-    var expresionRegular = /\b(libro|cuaderno|hoja|borrador|Estuche|separador|Grapa|resaltador|silicon|canionera|pizarron|Tarea|Bolson|China|Lapiz|ReglaT|Pipa|Goma|Regla|Escuadra|Alfiler|Tapon|Compas|Transportador|Limpiapipas|Corrector|Librillo|Cuadernillo|Agenda|Pin|Mina|Bolsa Clip|Aguja|Dispensador|Boligrafo|Folios|Ordenador|Tinta|Cartucho|rotulador|radio|mouse|ejercicio|Jabon|extension|papel|tableta|gel|grapadora|acuarela|pizarra|nota|algodon|mapa|PapelCrepe|tiza|PistolaSilicon|plasticina|pincel|carton|etiqueta|PostIf|sacapuntas|pegamento|calculadora)\b/g;
-    var regexTiposVariables
-
-    // Array para almacenar los tokens encontrados
+  
+    // Definir expresiones regulares para diferentes tipos de tokens
+    var T_AlcVariables = /\b(Libro|Cuaderno)\b/gi;
+    var T_Variables = /\b(Hoja|Borrador|Estuche|Separador|Grapa|Resaltador|Silicon)\b/gi;
+    var T_Impinfo = /\b(Canonera|Pizarron)\b/gi;
+    var T_ModificadorPu = /\b(Tarea)\b/gi;
+    var T_ModificadorPr = /\b(Bolson)\b/gi;
+    var T_ModificadorDe = /\b(China)\b/gi;
+    var T_ModificadoraPro = /\b(Lapiz)\b/gi;
+    var T_PalabrasClave = /\b(Boligrafo|Etiqueta|Sello|Resaltador|Rotulador|Radio|Mouse|Ejercicio|Jabon|Extensión|Papel|Tableta|Gel|Grapadora|Acuarela|Pizarra|Nota|Algodon|Mapa|PapelCrepe|Tiza|PistolaSilicon|Plasticina|Pincel|Cartón|Folder)\b/gi;
+    var T_Sentencias = /\b(Rotulador|Postif|Sacapuntas|Pegamento|Calculadora)\b/gi;
+    var T_Operadores = /[+-/*]/g; // Operadores aritméticos
+    var T_Digitos = /\b[0-9]+\b/g; // Dígitos del 0 al 9
+    var T_CaracteresEspeciales = /[!@#$%^&(),.?":{}|<>]/g;
+    var T_Comentario = /\/\/.*/g; // Comentario de una línea
+    var T_ComentarioMultiLinea = /\/\*[\s\S]*?\*\//g; // Comentario de varias líneas
+    var T_palabras = /\b(java|util|scanner|class|Documentocompiladores|static|main|system|out|Ingrese|in|el|primer|digito|num1|nextBorrador|segundo|num2|seleccione|la|operacion|a|realizar|suma|resta|division|multiplicacion|opcion|resultado|num2|num3|Error|numero|mayor|incorrecta|resultado|close|[0-9]|nextHoja|es|un|args)\b/gi;
+    // Array para almacenar los tokens encontrad
     var tokensEncontrados = [];
-
-    // Obtenemos las coincidencias con la expresión regular
-    var match;
-
-    while ((match = expresionRegular.exec(texto)) !== null) {
-        // Obtenemos la descripción del token
-        var descripcion = match[0];
-
-        // Obtenemos la posición del token en el texto
+    // Array para almacenar los errores encontrados
+    var errores = [];
+  
+    // Función para agregar tokens encontrados al array
+    function agregarTokens(regex, descripcion) {
+      var match;
+      while ((match = regex.exec(texto)) !== null) {
+        var valor = match[0];
         var inicio = match.index;
-        var fin = inicio + descripcion.length;
-
-        // Obtenemos el valor del token
-        var valor = texto.substring(inicio, fin);
-
-        // Obtenemos la línea de código donde se encuentra el token
+        var fin = inicio + valor.length;
         var lineaCodigo = obtenerNumeroLinea(texto, inicio);
-
-        // Creamos el objeto JSON con la información del token
-        var token = {
-            "token": valor,
-            "descripcion": descripcion,
-            "valor": valor,
-            "lineaCodigo": lineaCodigo
-        };
-        // Agregamos el token al array
+        var token = { "token": valor, "descripcion": descripcion, "lineaCodigo": lineaCodigo };
         tokensEncontrados.push(token);
+      }
     }
-    // Retornamos el array de tokens encontrados en formato JSON
-    console.log(JSON.stringify(tokensEncontrados))
+  
+    // Agregar tokens para cada expresión regular
+    agregarTokens(T_AlcVariables, "T_AlcanceVariables");
+    agregarTokens(T_Variables, "T_Variables");
+    agregarTokens(T_Impinfo, "T_Información Impresa");
+    agregarTokens(T_ModificadorPu, "T_Modificador Público");
+    agregarTokens(T_ModificadorPr, "T_Modificador Protegido");
+    agregarTokens(T_ModificadorDe, "T_Modificador Default");
+    agregarTokens(T_ModificadoraPro, "T_Modificador Privado");
+    agregarTokens(T_PalabrasClave, "T_Palabras Clave");
+    agregarTokens(T_Sentencias, "T_Sentencias");
+    agregarTokens(T_Digitos, "T_Dígitos");
+    agregarTokens(T_CaracteresEspeciales, "T_Caracteres Especiales");
+    agregarTokens(T_Comentario, "T_Comentario");
+    agregarTokens(T_ComentarioMultiLinea, "T_ComentarioMultiLinea");
+    
+    // Validar si las palabras coinciden con las expresiones regulares
+    var palabras = texto.match(/\b\w+\b/g) || [];
+    palabras.forEach(palabra => {
+        if (!palabra.match(T_AlcVariables) &&
+            !palabra.match(T_Variables) &&
+            !palabra.match(T_Impinfo) &&
+            !palabra.match(T_ModificadorPu) &&
+            !palabra.match(T_ModificadorPr) &&
+            !palabra.match(T_ModificadorDe) &&
+            !palabra.match(T_ModificadoraPro) &&
+            !palabra.match(T_PalabrasClave) &&
+            !palabra.match(T_palabras) &&
+            !palabra.match(T_Sentencias)) {
+            errores.push({ "token": palabra, "descripcion": "Error léxico: Palabra no reconocida en el contexto actual", "lineaCodigo": obtenerNumeroLinea(texto, texto.indexOf(palabra)) });
+        }
+    });
+  
+    // Retornar los tokens y errores encontrados en formato JSON
+    return { tokensEncontrados: tokensEncontrados, errores: errores };
+}
 
-    return JSON.stringify(tokensEncontrados);
+
+  function muestraTokens() {
+    const { tokensEncontrados, errores } = validarTexto();
+    setTokens(tokensEncontrados || []);
+    setErroresLexicos(errores || []);
   }
 
   // Función para obtener el número de línea de un texto dado una posición
@@ -72,43 +109,37 @@ function App() {
       return lineas.length;
   }
 
-  function muestraTokens(){
-    const jsonValidaciones = validarTexto();
 
-    setTokens(JSON.parse(jsonValidaciones));
-  }
-
-
-  return ( 
-  <>
-    <div className="div-padre">  
+ return (
+    <>
+      <div className="div-padre">
         <div>
           <FileUploader onFileUpload={onFileUpload} />
         </div>
-    
-        <div className="superior" style={{padding: 0, margin: 0, alignContent: "initial"}}>
-            <h3 style={{color:"white"}}>Code Area</h3>
-            <AceEditor
-                    ref={editorRef}
-                    mode="csharp" // Lenguaje de programación
-                    theme="monokai" // Tema del editor
-                    onChange={onFileUpload} // Función para manejar cambios en el código
-                    value={fileContent} // Valor del código en el editor
-                    editorProps={{ $blockScrolling: true, }} // Propiedades del editor
-                    fontSize={17}
-                    style={{ width: '100%', height: '80%'}} // Estilo del editor
-                    setOptions={{
-                      enableBasicAutocompletion: true,
-                      enableLiveAutocompletion: true,
-                      enableSnippets: true
-                    }}
-            />
+
+        <div className="superior" style={{ padding: 0, margin: 0, alignContent: "initial" }}>
+          <h3 style={{ color: "white" }}>Code Area</h3>
+          <AceEditor
+            ref={editorRef}
+            mode="csharp"
+            theme="monokai"
+            onChange={onFileUpload}
+            value={fileContent}
+            editorProps={{ $blockScrolling: true, }}
+            fontSize={17}
+            style={{ width: '100%', height: '80%' }}
+            setOptions={{
+              enableBasicAutocompletion: true,
+              enableLiveAutocompletion: true,
+              enableSnippets: true
+            }}
+          />
         </div>
-    
-        <div style={{ overflowX: 'auto'}} className="inferior-izquierda">
-          <Container style={{display: "flex", justifyContent: "center", alignContent:"center"}}>
-            <h1 style={{color:"white", paddingRight: "100px"}}>Tabla Errores</h1>
-            <button onClick={muestraTokens} className="btn btn-secondary" style={{height: "35px"}}>Ejecutar</button>
+
+        <div style={{ overflowX: 'auto' }} className="inferior-izquierda">
+          <Container style={{ display: "flex", justifyContent: "center", alignContent: "center" }}>
+            <h1 style={{ color: "white", paddingRight: "100px" }}>Tabla Errores</h1>
+            <button onClick={muestraTokens} className="btn btn-secondary" style={{ height: "35px" }}>Ejecutar</button>
           </Container>
 
           <table style={{ borderCollapse: "collapse", width: "95%" }}>
@@ -116,28 +147,27 @@ function App() {
               <tr>
                 <th style={{ border: "1px solid white", padding: "3px" }}>Token</th>
                 <th style={{ border: "1px solid white", padding: "3px" }}>Descripción</th>
-                <th style={{ border: "1px solid white", padding: "3px" }}>Valor</th>
                 <th style={{ border: "1px solid white", padding: "3px" }}>Línea de Código</th>
               </tr>
             </thead>
             <tbody>
-              {/* Mapeamos los tokens para renderizar filas de la tabla */}
-              {tokens.map((token, index) => (
+              {/* Mapeo de errores léxicos */}
+              {erroresLexicos.map((error, index) => (
                 <tr key={index}>
-                  <td style={{ border: "1px solid white", padding: "3px" }}>{token.token}</td>
-                  <td style={{ border: "1px solid white", padding: "3px" }}>{token.descripcion}</td>
-                  <td style={{ border: "1px solid white", padding: "3px" }}>{token.valor}</td>
-                  <td style={{ border: "1px solid white", padding: "3px" }}>{token.lineaCodigo}</td>
+                  <td style={{ border: "1px solid white", padding: "3px" }}>{error.token}</td>
+                  <td style={{ border: "1px solid white", padding: "3px" }}>{error.descripcion}</td>
+                  <td style={{ border: "1px solid white", padding: "3px" }}>{error.lineaCodigo}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
 
-        <div style={{ overflowX: 'auto'}} className="inferior-derecha" >
-          <Container style={{display: "flex", justifyContent: "center"}}>
-            <h1 style={{color:"white", paddingRight: "100px"}}>Tabla de Tokens</h1>
-            <button className="btn btn-secondary" style={{height: "35px"}}>Ejecutar</button>
+        {/* Sección de tokens */}
+        <div style={{ overflowX: 'auto' }} className="inferior-derecha" >
+          <Container style={{ display: "flex", justifyContent: "center" }}>
+            <h1 style={{ color: "white", paddingRight: "100px" }}>Tabla de Tokens</h1>
+            <button className="btn btn-secondary" style={{ height: "35px" }}>Ejecutar</button>
           </Container>
 
           <table style={{ borderCollapse: "collapse", width: "95%" }}>
@@ -145,25 +175,23 @@ function App() {
               <tr>
                 <th style={{ border: "1px solid white", padding: "3px" }}>Token</th>
                 <th style={{ border: "1px solid white", padding: "3px" }}>Descripción</th>
-                <th style={{ border: "1px solid white", padding: "3px" }}>Valor</th>
                 <th style={{ border: "1px solid white", padding: "3px" }}>Línea de Código</th>
               </tr>
             </thead>
             <tbody>
-              {/* Mapeamos los tokens para renderizar filas de la tabla */}
+              {/* Mapeo de tokens */}
               {tokens.map((token, index) => (
                 <tr key={index}>
                   <td style={{ border: "1px solid white", padding: "3px" }}>{token.token}</td>
                   <td style={{ border: "1px solid white", padding: "3px" }}>{token.descripcion}</td>
-                  <td style={{ border: "1px solid white", padding: "3px" }}>{token.valor}</td>
                   <td style={{ border: "1px solid white", padding: "3px" }}>{token.lineaCodigo}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
-    </div>
-  </>
+      </div>
+    </>
   );
 }
 
